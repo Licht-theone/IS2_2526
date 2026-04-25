19ac3feae58bdee2b7dd2a712698814795b00094
Aaron <198075048+Licht-theone@users.noreply.github.com>
2026-04-25 13:48:23 +0200

    Finalizados los test de integracion para el caso de uso implementado

--- practica5/SegurosGUI/src/test/java/TestITVistaAgente.java
+++ practica5/SegurosGUI/src/test/java/TestITVistaAgente.java
@@ -1,3 +1,5 @@
+import java.awt.Dimension;
+
 import org.assertj.swing.fixture.FrameFixture;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,25 +20,36 @@ public class TestITVistaAgente {
 	public void setUp() {
 		IClientesDAO clientesDAO = new ClientesDAO();
 		ISegurosDAO segurosDAO = new SegurosDAO();
-		
 		GestionSeguros gestion = new GestionSeguros(clientesDAO, segurosDAO);
 		vista = new VistaAgente(gestion, gestion, gestion);
 		ventana = new FrameFixture(vista);
-		ventana.show();
+		ventana.show(new Dimension(450, 341));
 	}
 	
 	@AfterEach
-	public void cleanUp() {
-		ventana.cleanUp();
+	public void tearDonw() {
+		if (ventana != null) {
+			ventana.cleanUp();
+		}	
 	}
 	
 	@Test
 	public void testBuscarClienteNoValido() {
 		//Para probar el caso de uso consulta cliente se prueba con uno que exista y otro que no
+		ventana.textBox("txtDNICliente").enterText("99999999Z");
+		ventana.button("btnBuscar").click();
+		ventana.textBox("txtNombreCliente").requireText("Error en BBDD");
+		ventana.list().requireItemCount(0);
+		ventana.textBox("txtTotalCliente").requireEmpty();
 	}
 	
 	@Test
 	public void testBuscarClienteValido() {
-		
+		//busca el cliente valido
+		ventana.textBox("txtDNICliente").enterText("11111111A");
+		ventana.button("btnBuscar").click();
+		ventana.textBox("txtNombreCliente").requireText("Juan");
+		ventana.list().requireItemCount(3);
+		ventana.textBox("txtTotalCliente").requireText("1820.0");
 	}
 }
