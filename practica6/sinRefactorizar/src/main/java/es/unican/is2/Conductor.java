af5e9aef5d9aefa805f85f3c6cd34fa82f8f3259
Aaron <198075048+Licht-theone@users.noreply.github.com>
2026-05-10 16:52:32 +0200

    clase conductor analizada

--- practica6/sinRefactorizar/src/main/java/es/unican/is2/Conductor.java
+++ practica6/sinRefactorizar/src/main/java/es/unican/is2/Conductor.java
@@ -2,6 +2,22 @@ package es.unican.is2;
 
 import java.util.ArrayList;
 
+
+/**
+ *  El wmc de esta clase se calcula con la formula de la suma de las complejidades ciclomaticas de sus metodos
+ *  tal y como se indica en las transparencias de clase
+ *  La complejidad ciclomatica de cada metodo es 1 + el numero de nodos de condicion (if, for, while, case, catch)
+ *  +1 por cada operador && o ||
+ *  WMC=5+1+1+1+1+1+1+6+1=18
+ *  WMCn=18/9=2
+ *  CCog de la clase = 9
+ *  CBO= usa la clase transporte,categoriatransporte e illegalargumentexception=3
+ *  DIT=0 no hereda ninguna clase
+ *  NOC=0 no hay clase que herede de esta
+ */
+
+
+
 /**
  * Clase que representa a un conductor, con sus datos personales
  * y los transportes que ha realizado. 
@@ -15,6 +31,8 @@ public class Conductor {
 	private String apellido2;
 	private String dire;
 
+	//cc=1+if+or+or+or=5
+	//ccog=+1 por el if +1 por los or=2
 	public Conductor(String dni, String nombre, String apellido1,
 			String apellido2, String direccion) {
 		if (dni == null || nombre == null || apellido1 == null || direccion == null) {
@@ -27,30 +45,44 @@ public class Conductor {
 		this.dire = direccion;
 	}
 
+	//cc=1
+	//ccog=0
 	public String dni() {
 		return dni;
 	}
-
+	
+	//cc=1
+	//ccog=0
 	public String getDni() {
 		return dni;
 	}
 
+	//cc=1
+	//ccog=0
 	public String getNombre() {
 		return nombre;
 	}
 
+	//cc=1
+	//ccog=0
 	public String getApellido1() {
 		return apellido1;
 	}
 
+	//cc=1
+	//ccog=0
 	public String apellido2() {
 		return apellido2;
 	}
 
+	//cc=1
+	//ccog=0
 	public String getDire() {
 		return dire;
 	}
-
+	
+	//CC= 1+for+case+case+case+if=6
+	//ccog=+1(for)+2(switch anidado en el for)+3(if anidado en switch anidado en for)+1(else)=7
 	public double sueldo() {
 		double sueldoTransportes = 0;
 		for (Transporte t : transportes) {
@@ -74,6 +106,8 @@ public class Conductor {
 		return 700 + sueldoTransportes;
 	}
 
+	//cc=1
+	//ccog=0
 	public void anhadeTransporte(Transporte t) {
 		transportes.add(t);
 	}
